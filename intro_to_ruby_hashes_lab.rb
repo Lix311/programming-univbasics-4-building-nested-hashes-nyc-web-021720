1c2952be68bd07d3f94579951b14e15c34afe240
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-05 01:02:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,11 +27,10 @@ def monopoly_with_third_tier
         :b_and_o_railroad => {},
         :reading_railroad => {},
         :short_line_railroad => {}
-        
-      },
+        },
       :pieces => 4,
       :rent_in_dollars => {
-        :key1 => {},
+        :one_piece_owned => {25},
         :key2 => {},
         :key3 => {},
         :key4 => {}
